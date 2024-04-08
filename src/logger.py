80fe60d47cc6b5f803c41eca1f53767409c92dc4
Sneha Singh <snehasinggh65@gmail.com>
2024-04-08 23:14:09 +0530

    components

--- src/logger.py
+++ src/logger.py
@@ -3,7 +3,7 @@ import os
 from datetime import datetime 
 
 
-LOG_FILE = f"{datetime.now().strftime("%m_%d_%Y_%H_%M_%S")}.log"
+LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
 log_path = os.path.join(os.getcwd(), "logs", LOG_FILE)
 os.makedirs(log_path, exist_ok= True)
 
